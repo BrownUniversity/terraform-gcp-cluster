e245f1e63443a455def6e030131295547e23bd33
Isabel Restrepo <isabelrestre@gmail.com>
2020-07-09 14:42:54 -0400

    fix: restrict version of google-api
    See [this](https://github.com/terraform-providers/terraform-provider-google/issues/6744)

--- version.tf
+++ version.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 0.12"
 
   required_providers {
-    google      = ">= 3.0, <4.0.0"
-    google-beta = ">= 3.0, <4.0.0"
+    google      = ">= 3.0, <3.29.0"
+    google-beta = ">= 3.0, <3.29.0"
   }
 }
+
