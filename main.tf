4eafb5ed9bb6b5ee15d4fe3d3b66f0ac426ec9de
Isabel Restrepo <isabelrestre@gmail.com>
2022-06-03 14:53:39 -0400

    fix: remove version as it's now in url

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@
 # Create the GKE Cluster
 module "gke" {
   source                     = "git::git@github.com:terraform-google-kubernetes-engine/terraform-google-kubernetes-engine.git//modules/beta-private-cluster?ref=v15.0.1"
-  version                    = "15.0.1"
   project_id                 = var.project_id
   name                       = var.cluster_name
   regional                   = var.regional
