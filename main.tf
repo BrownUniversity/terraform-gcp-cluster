0b534fa9d9b0af300702150b17b580134d567e6e
Sean Broestl <broestls@gmail.com>
2022-07-15 09:18:23 -0400

    Call specific GKE version in release channel

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 module "gke" {
   source                     = "terraform-google-modules/kubernetes-engine/google//modules/beta-private-cluster"
   version                    = "15.0.1"
-  kubernetes_version         = "1.22"
+  kubernetes_version         = "1.22.8-gke.202"
   project_id                 = var.project_id
   name                       = var.cluster_name
   regional                   = var.regional
