179f7b1ac35da7e0874ed63f2da6e4ed13a10bb0
Josh <joshua_lu1@brown.edu>
2025-01-07 14:30:39 -0500

    update versioning

--- tests/simple-cluster/main.tf
+++ tests/simple-cluster/main.tf
@@ -15,7 +15,7 @@ locals {
 # ------------------------------------------------------------
 # Create the GCP Project
 module "project" {
-  source          = "git::https://github.com/BrownUniversity/terraform-gcp-project.git?ref=v0.1.6"
+  source          = "git::https://github.com/BrownUniversity/terraform-gcp-project.git?ref=v0.1.7"
   project_name    = local.project_name
   org_id          = var.org_id
   billing_account = var.billing_account
--- tests/simple-cluster/version.tf
+++ tests/simple-cluster/version.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = ">= 1.9.2"
+  required_version = ">= 1.10.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.38.0"
+      version = "6.15.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "5.38.0"
+      version = "6.15.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.6.2"
+      version = "3.6.3"
     }
   }
 }
