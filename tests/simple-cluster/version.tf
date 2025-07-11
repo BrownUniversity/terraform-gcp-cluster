c986cb8ee7c32c5776a5480dfb718859489e1a40
Camilo Diaz <k.diaz99@gmail.com>
2025-07-11 10:15:01 -0400

    maint: updating terrafom and google modules versions. adding dependabot

--- tests/simple-cluster/version.tf
+++ tests/simple-cluster/version.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = ">= 1.10.0"
+  required_version = ">= 1.12.2"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "6.15.0"
+      version = "6.42.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "6.15.0"
+      version = "6.42.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.6.3"
+      version = "3.7.2"
     }
   }
 }
